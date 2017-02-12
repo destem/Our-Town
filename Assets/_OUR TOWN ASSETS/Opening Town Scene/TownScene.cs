ec9f238b09b783d60da46aa3010eb1ddae268fcb
Marc - home <marc.destefano@gmail.com>
2017-02-12 13:05:56 -0500

    Collect UV coords by clicking, sequenced house outlines

--- Assets/_OUR TOWN ASSETS/Opening Town Scene/TownScene.cs
+++ Assets/_OUR TOWN ASSETS/Opening Town Scene/TownScene.cs
@@ -66,9 +66,6 @@ public class TownScene : MonoBehaviour {
         ResetUVs();
         next = false;
            
-        Vector3 mousePos = Input.mousePosition;
-        t.text = string.Format("{0:0.000}, {1:0.000}", mousePos.x / Screen.width, mousePos.y / Screen.height);
-
         if (Input.GetButtonDown("Jump"))
         {
             next = true;
@@ -107,16 +104,7 @@ public class TownScene : MonoBehaviour {
             yield return null;
         }
         next = false;
-        print("first houses");
-        growMat.SetTexture("_MaskOneTex", MaskOneTex);
-        growMat.SetTexture("_MaskTwoTex", MaskTwoTex);
-        SetMaskOne(.465f, .563f);
-        yield return new WaitForSeconds(2.0f);
-        SetMaskTwo(.464f, .565f);
-        yield return new WaitForSeconds(1.5f);
-        SetMaskOne(.526f, .565f);
-        yield return new WaitForSeconds(2.0f);      
-        SetMaskTwo(.524f, .557f);
+        StartCoroutine(FirstHouses());
         while (!next)// && !gesture.IsClap())
         {
             Blit();
@@ -126,36 +114,60 @@ public class TownScene : MonoBehaviour {
         StartCoroutine(HouseOutlines());
     }
 
+    IEnumerator FirstHouses()
+    {
+        print("first houses");
+        growMat.SetTexture("_MaskOneTex", MaskOneTex);
+        growMat.SetTexture("_MaskTwoTex", MaskTwoTex);
+        SetMaskOne(.465f, .563f);
+        yield return new WaitForSeconds(2.0f);
+        SetMaskTwo(.464f, .565f);
+        yield return new WaitForSeconds(1.5f);
+        SetMaskOne(.526f, .565f);
+        yield return new WaitForSeconds(2.0f);
+        SetMaskTwo(.524f, .557f);
+    }
+
     IEnumerator HouseOutlines()
     {
         float outlineDelay = 2f;
         //outlines are all on mask one. starting in lower-right and working left
-        /*
-
-         */
+        float[] coords = { 0.353f, 0.998f, 0.933f, 0.024f, 0.931f, 0.446f, 0.888f, 0.446f, 0.868f, 0.038f,
+                           0.848f, 0.158f, 0.859f, 0.630f, 0.823f, 0.596f, 0.786f, 0.699f, 0.740f, 0.024f,
+                           0.709f, 0.473f, 0.765f, 0.226f, 0.668f, 0.013f, 0.724f, 0.020f, 0.657f, 0.473f,
+                           0.631f, 0.583f, 0.613f, 0.583f, 0.657f, 0.140f, 0.573f, 0.274f, 0.471f, 0.027f,
+                           0.491f, 0.027f, 0.461f, 0.099f, 0.414f, 0.013f, 0.370f, 0.566f, 0.417f, 0.504f,
+                           0.298f, 0.027f, 0.299f, 0.514f, 0.273f, 0.024f, 0.241f, 0.504f, 0.280f, 0.507f,
+                           0.197f, 0.020f, 0.184f, 0.541f, 0.141f, 0.034f, 0.132f, 0.686f, 0.105f, 0.466f,
+                           0.078f, 0.579f, 0.074f, 0.253f, 0.025f, 0.363f, 0.034f, 0.404f };
         print("starting house outlines");
-        SetMaskOne(.933f, .009f);
-        yield return new WaitForSeconds(outlineDelay);
-        SetMaskOne(.931f, .445f);
-        yield return new WaitForSeconds(outlineDelay);
-        SetMaskOne(.888f, .44f);
-        yield return new WaitForSeconds(outlineDelay);
-        SetMaskOne(.869f, .009f);
-        yield return new WaitForSeconds(outlineDelay);
-        SetMaskOne(.859f, .604f);
-        yield return new WaitForSeconds(outlineDelay);
-        SetMaskOne(.848f, .15f);
-        yield return new WaitForSeconds(outlineDelay);
-        SetMaskOne(.823f, .59f);
-        yield return new WaitForSeconds(outlineDelay);
-        SetMaskOne(.785f, .7f);
-        yield return new WaitForSeconds(outlineDelay);
-        SetMaskOne(.74f, .005f);
-        yield return new WaitForSeconds(outlineDelay);
-        SetMaskOne(.725f, .452f);
-        yield return new WaitForSeconds(outlineDelay);
-        SetMaskOne(.69f, .005f);
-        yield return new WaitForSeconds(outlineDelay);
+        for (int i = 0; i< coords.Length; i += 2)
+        {
+            SetMaskOne(coords[i], coords[i + 1]);
+            yield return new WaitForSeconds(outlineDelay);
+        }
+        //SetMaskOne(.933f, .009f);
+        //yield return new WaitForSeconds(outlineDelay);
+        //SetMaskOne(.931f, .445f);
+        //yield return new WaitForSeconds(outlineDelay);
+        //SetMaskOne(.888f, .44f);
+        //yield return new WaitForSeconds(outlineDelay);
+        //SetMaskOne(.869f, .009f);
+        //yield return new WaitForSeconds(outlineDelay);
+        //SetMaskOne(.859f, .604f);
+        //yield return new WaitForSeconds(outlineDelay);
+        //SetMaskOne(.848f, .15f);
+        //yield return new WaitForSeconds(outlineDelay);
+        //SetMaskOne(.823f, .59f);
+        //yield return new WaitForSeconds(outlineDelay);
+        //SetMaskOne(.785f, .7f);
+        //yield return new WaitForSeconds(outlineDelay);
+        //SetMaskOne(.74f, .005f);
+        //yield return new WaitForSeconds(outlineDelay);
+        //SetMaskOne(.725f, .452f);
+        //yield return new WaitForSeconds(outlineDelay);
+        //SetMaskOne(.69f, .005f);
+        //yield return new WaitForSeconds(outlineDelay);
     }
 
     IEnumerator Roofs()
---
+++ Assets/_OUR TOWN ASSETS/_General/CollectUVs.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+public class CollectUVs : MonoBehaviour
+{
+
+    List<float> coords;
+    public Material RIMat;
+    public Texture2D TextoView;
+
+    // Use this for initialization
+    void Start()
+    {
+        coords = new List<float>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            coords.Add(mousePos.x / Screen.width);
+            coords.Add(mousePos.y / Screen.height);
+            //theString += string.Format("{0:0.000}, {1:0.000}", mousePos.x / Screen.width, mousePos.y / Screen.height);
+            EditorGUIUtility.systemCopyBuffer = "{" + string.Join(", ", coords.ConvertAll(i => string.Format("{0:0.000}f", i)).ToArray()) + "}";
+
+        }
+    }
+    void OnRenderImage(RenderTexture source, RenderTexture dest)
+    {
+        Graphics.Blit(TextoView, dest, RIMat);
+    }
+}
