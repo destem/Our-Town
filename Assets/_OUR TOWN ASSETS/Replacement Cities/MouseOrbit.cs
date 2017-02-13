b1f306bf70aa9a8fab8b152bd7dca117b3fd7b4f
Marc - home <marc.destefano@gmail.com>
2017-02-13 15:33:04 -0500

    Added "Replacement Cities" demo

---
+++ Assets/_OUR TOWN ASSETS/Replacement Cities/MouseOrbit.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOrbit : MonoBehaviour {
+
+    public float RotateAmount = 15f;
+
+
+    void LateUpdate()
+    {
+        OrbitCamera();
+    }
+
+    public void OrbitCamera()
+    {
+        //bool isCtrlKeyDown = Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl);
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 target = Vector3.zero; //this is the center of the scene, you can use any point here
+            float y_rotate = Input.GetAxis("Mouse X") * RotateAmount;
+            float x_rotate = Input.GetAxis("Mouse Y") * RotateAmount;
+            OrbitCamera(target, y_rotate, x_rotate);
+        }
+    }
+
+    public void OrbitCamera(Vector3 target, float y_rotate, float x_rotate)
+    {
+        Vector3 angles = transform.eulerAngles;
+        angles.z = 0;
+        transform.eulerAngles = angles;
+        transform.RotateAround(target, Vector3.up, y_rotate);
+        transform.RotateAround(target, Vector3.left, x_rotate);
+
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/_OUR TOWN ASSETS/Replacement Cities/PingPongDistance.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingPongDistance : MonoBehaviour {
+
+    Transform[] children;
+    float count;
+    float t;
+    public float min = .005f;
+    public float max = .2f;
+    public float pingPongtime = 10f;
+
+	// Use this for initialization
+	void Start () {
+        children = GetComponentsInChildren<Transform> ();
+        count = transform.childCount;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(1))
+        {
+            t = Input.mousePosition.x / 2000f;
+            t = Mathf.Clamp(t, min, max);
+            for (int i = 0; i <= count; i++)
+            {
+                children[i].localPosition = new Vector3(0f, 0f, -i * t + count * t / 2f);
+            }
+        }
+        //t = Mathf.Lerp (min, max, Mathf.PingPong (Time.time / pingPongtime, 1f));
+        
+	}
+}
+    
