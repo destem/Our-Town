f6551bf9d9d5ae189f8acd14d6de4aa0e80c8cdc
Marc - home <marc.destefano@gmail.com>
2017-02-24 11:53:54 -0500

    added old MicListener code just in case

---
+++ Assets/_OUR TOWN ASSETS/Microphone/MicListener.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class MicListener : MonoBehaviour {
+	
+	public float sensitivity = 100;
+	public float loudness = 0;
+	AudioSource sound;
+	bool micExists = false;
+	string micName;
+	int i; //counter
+
+	float[] soundData = new float[256];
+	float soundSum = 0;
+
+	Text micText;
+
+	void Start() {
+		if (Microphone.devices.Length > 0){
+			//TODO: search for (create?) mixer, search for (create?) new group, set group vol to -80dB
+
+			micExists = true;
+			micName = Microphone.devices[0];
+			print ("we have a mic and its name is " + micName);
+			sound = GetComponent<AudioSource> ();
+			sound.clip = Microphone.Start(micName, true, 10, 44100); // theoretically should loop over itself, so we don't need to flush RAM
+			sound.loop = true; // Set the AudioClip to loop
+			while (!(Microphone.GetPosition(micName) > 0)){} // Wait until recording starts
+			sound.Play(); // Play the audio source so that we can check its volume
+			print ("Recording");
+		}
+		micText = GameObject.Find ("MicText").GetComponent<Text> ();
+	}
+
+	void Update(){
+		if (micExists) {
+			loudness = GetAveragedVolume () * sensitivity;
+			if (micText) {
+				micText.text = "Loudness: " + loudness;
+			}
+		}
+	}
+
+	float GetAveragedVolume()
+	{ 
+		soundSum = 0f;
+		sound.GetOutputData(soundData,0);
+		for (i=0; i< soundData.Length; i++)
+		{
+			soundSum += Mathf.Abs(soundData[i]);
+		}
+		return soundSum/256;
+	}
+}
